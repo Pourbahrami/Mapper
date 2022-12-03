525d1d28902eb1ff87f6cbabc6c0dddf207731ef
Hossein Pourbahrami <hossein.pourbahrami@gmail.com>
2022-12-03 23:49:05 +0330

    initial commit

---
+++ src/Mapper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pourbahrami\Mapper;
+
+use Illuminate\Support\Arr;
+
+class Mapper
+{
+    private $input;
+
+    function __construct(array $input)
+    {
+        $this->input = $input;
+    }
+
+    static public function createFromJson($json): Mapper
+    {
+        return new Mapper(json_decode($json, true));
+    }
+
+    static public function createFromXml($xml): Mapper
+    {
+        return new Mapper(json_decode(json_encode(simplexml_load_string($xml)), true));
+    }
+
+    public function map(array $map)
+    {
+        return $this->remap($this->input, $map);
+    }
+
+    private function remap($value, array $map, $parents = '')
+    {
+        if (is_array($value)) {
+            $newArr = [];
+            foreach ($value as $key => $val) {
+                if (!is_numeric($key)) {
+                    $fullKey = $parents ? $parents . '.' . $key : $key;
+                    $newKey = isset($map[$fullKey]) ? $map[$fullKey] : $key;
+                    $newArr[$newKey] = $this->remap($val, $map, $fullKey);
+                } else {
+                    $newArr[$key] = $this->remap($val, $map, $parents);
+                }
+            }
+            return $newArr;
+        } else {
+            return $value;
+        }
+    }
+}
